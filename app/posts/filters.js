989ce9b047d4131b45dac6e9525dd1c12ba3e163
Michael Treser <armikael2@gmail.com>
2015-02-01 22:31:55 +0300

    Adding dynamic links for sidebar filters: Categories, Authors and Months.

--- app/posts/filters.js
+++ app/posts/filters.js
@@ -5,30 +5,6 @@
 
 	var tag = 0;
 
-	// Creating Custom filter
-	app.filter('searchTag', function () {
-		return function (arr, searchText) {
-			console.log(arr);
-			console.log(searchText);
-
-			for (var i = 0; i < arr.length; i++) {
-				if (arr[i] === searchText) {
-					tag = tag++;
-				}
-			}
-
-			return tag;
-
-			console.log('tag', tag);
-			console.log(arr);
-
-			// return arr.filter(function (item) {
-			// 	// return item.indexOf(searchText);
-			// 	console.log(item);
-			// })
-		}
-	});
-
 	app.filter('replaceSpaces', function() {
 		return function(link){
 			var dashedLink = link.replace(/(\s|\W)/g, '-').toLowerCase();
