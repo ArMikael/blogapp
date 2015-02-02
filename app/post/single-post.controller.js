00414007923d3a2cd10c8e92521629bf4f9b7dce
Michael Treser <armikael2@gmail.com>
2015-02-02 14:18:27 +0300

    Exporting Sidebar html to separate view file.

--- app/post/single-post.controller.js
+++ app/post/single-post.controller.js
@@ -5,8 +5,33 @@
 
 	app.controller('SinglePostCtrl', ['$scope', '$routeParams', 'postsData',
 		function ($scope, $routeParams, postsData) {
-			console.log($routeParams);
+			console.log('$routeParams from SinglePostCtrl', $routeParams);
 			console.log(postsData);
+
+			postsData.success(function (data, status) {
+
+				// initialize the scope with the JSON data object
+				$scope.postsData = data.posts;
+
+				for (var post in $scope.postsData) {
+
+					if ($scope.postsData.hasOwnProperty(post)) {
+
+						var postTitle = $scope.postsData[post].title;
+						console.log('postTitle before cleaning', postTitle);
+						console.log(replaceSpaces(postTitle));
+
+						if (postTitle === $routeParams.title){
+
+							$scope.post = $scope.postsData[post];
+						}
+					}
+				}
+
+			})
+			.error(function (data , status) {
+				console.erorr(status, data);
+			});
 
 
 			// $scope.post = dataService.get($routeParams.title);
